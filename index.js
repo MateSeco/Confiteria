841399ffeefdb88eba65cf389349a9389ccbb6b0
Nacho A <48703982+MateSeco@users.noreply.github.com>
2021-01-04 12:02:12 -0300

    comment

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set("views", path.join(__dirname, "views")); //Setting up default view path
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+//comment
 //seeder();
 routes(app);
 

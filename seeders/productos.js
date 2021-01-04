ec385ca1c58bb9d32d698983ca475a2e1fbf2934
Nacho A <48703982+MateSeco@users.noreply.github.com>
2021-01-04 17:28:33 -0300

    descargo fotos para productos

--- seeders/productos.js
+++ seeders/productos.js
@@ -1,6 +1,17 @@
 const productos = [
-  { name: "Producto 1", description: "La primera descripcion", price: 10 },
-  { name: "Producto 2", description: "La segunda descripcion", price: 15 },
+  {
+    name: "Merenguitos ",
+    description:
+      "Pequeños deliciosos merengues con la cantidad justa de chocolate. Crocantes por fuera con un centro suave que se derrite en la boca.",
+    price: 10,
+    image: "images/merengues.jpg",
+  },
+  {
+    name: "Masas secas ",
+    description:
+      "Bandeja con 5 variedades de masas secas que aportan diferentes texturas y sabores, pasando de las crocantes almendras hasta los suaves MeliFAv. Variedades: Sablee, chocolate, sablee con naranja, almendritas, MeliFav",
+    price: 15,
+  },
 ];
 
 module.exports = productos;
